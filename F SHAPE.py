978fca21fdc8bb1dc655fbd544d9bd15ed3a1031
Sai-Sumanth-D <70420743+Sai-Sumanth-D@users.noreply.github.com>
2020-09-01 13:06:43 +0530

    Created a python code for forming a letter 'F'

---
+++ F SHAPE.py
@@ -0,0 +1,12 @@
+#converting the given number in strings to shape
+
+
+#numbers = [5,2,5,2,2]
+
+given_numbers = [7, 2, 5, 2, 2]
+
+for x_count in given_numbers :
+    output = " "
+    for count in range(x_count):
+        output += 'x'
+    print(output)
